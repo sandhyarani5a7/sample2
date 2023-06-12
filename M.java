405812766d844d82f004561547f3ab6b7daff150
sandhyarani5a7 <5a7sandhyarani@gmail.com>
2023-06-12 10:04:29 +0530

    making changes

--- M.java
+++ M.java
@@ -4,14 +4,16 @@ import javax.swing.JFrame;
 
 public class M 
 {
-
+		
 	public static void main(String[] args) 
 	{
 	
 		 Sub_2 x=new  Sub_2();
 		 x.sandhya();
+		 System.out.println("asguaihsjkjdhskda");
 		
 
 	}//close main
+	
 
 }//close class
